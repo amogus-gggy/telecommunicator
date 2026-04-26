5a6bdc8318112e562b6b17c1feff3ab042a6c6e1
BoodyWin <boodywin.official@gmail.com>
2026-04-26 09:15:28 +0300

    Updated to work with the login.py and register.py updated

--- client/src/main.py
+++ client/src/main.py
@@ -4,40 +4,22 @@ import views
 
 async def main(page: ft.Page):
     page.title = "Telecommunicator"
+    page.vertical_alignment=ft.MainAxisAlignment.CENTER
+    page.horizontal_alignment=ft.CrossAxisAlignment.CENTER
+
     page.bgcolor = cnst.COL_BACKGROUND
 
-    page.vertical_alignment = ft.MainAxisAlignment.CENTER
-    page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
-
-    def route_changed(e):
+    async def route_changed(e):
         page.views.clear()
-        # Use .startswith or normalized strings to be safe
-        sel_route = page.route.strip()
+        page.controls.clear()
+        if page.route == "/auth/login":
+            page.views.append(await views.login.login_ui(page))
+        elif page.route == "/auth/register":
+            page.views.append(await views.register.register_ui(page))
         
-        if sel_route == "/" or sel_route == "":
-            page.go("/login")
-            return
-
-        if sel_route == "/login":
-            new_view = views.login.main(page)
-            page.views.append(new_view)
-            if len(page.views) > 1:
-                page.views.pop(0)
-        elif sel_route == "/register":
-            new_view = views.register.main(page)
-            page.views.append(new_view)
-            if len(page.views) > 1:
-                page.views.pop(0)
-        
-        print(f"Target Route: {sel_route} | Views in stack: {len(page.views)}")
         page.update()
-
+    
     page.on_route_change = route_changed
-    # Start the app
-    if page.route == "/":
-        page.go("/login")
-    else:
-        page.update()
-
+    await page.push_route("/auth/login")
 
 ft.run(main)
