43e756f3b65a847949235f4e830f2021608a7ff4
VxidDev <stas050595@gmail.com>
2026-04-26 18:35:55 +0200

    Reverted config changes

--- client/config.py
+++ client/config.py
@@ -1,8 +1,5 @@
 """Client configuration."""
 
 # API settings
-# API_URL = "http://193.124.117.242:8000"
-# WS_URL = "ws://193.124.117.242:8000/ws"
-
-API_URL = "http://127.0.0.1:8000"
-WS_URL = "ws://127.0.0.1:8000/ws"
+API_URL = "http://193.124.117.242:8000"
+WS_URL = "ws://193.124.117.242:8000/ws"
