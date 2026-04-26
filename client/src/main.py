56545c08276899f3691a1e9da689d1f944bfa4b8
BoodyWin <boodywin.official@gmail.com>
2026-04-26 09:56:58 +0300

    Edited  transitions and added basic routing

--- client/src/main.py
+++ client/src/main.py
@@ -1,5 +1,6 @@
 import flet as ft
 import assets.constants as cnst
+from time import time as currentTime
 import views
 
 async def main(page: ft.Page):
@@ -8,14 +9,25 @@ async def main(page: ft.Page):
     page.horizontal_alignment=ft.CrossAxisAlignment.CENTER
 
     page.bgcolor = cnst.COL_BACKGROUND
+    page.theme = ft.Theme(
+        page_transitions=ft.PageTransitionsTheme(
+            android=ft.PageTransitionTheme.CUPERTINO,
+            ios=ft.PageTransitionTheme.CUPERTINO,
+            macos=ft.PageTransitionTheme.NONE,
+            linux=ft.PageTransitionTheme.NONE,
+            windows=ft.PageTransitionTheme.NONE,
+        )
+    )
 
     async def route_changed(e):
         page.views.clear()
 
         if page.route == "/auth/login":
-            page.views.append(await views.login.login_ui(page))
+            view = await views.login.login_ui(page)
+            page.views.append(view)
         elif page.route == "/auth/register":
-            page.views.append(await views.register.register_ui(page))
+            view = await views.register.register_ui(page)
+            page.views.append(view)
         
         page.update()
     
